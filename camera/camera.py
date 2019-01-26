eea02bababd7243d24fc1d26331aa724029ad305
François Blondel <fblondel@intelliad.de>
2019-01-26 14:26:18 +0100

    camera

--- camera/camera.py
+++ camera/camera.py
@@ -4,5 +4,7 @@ from time import sleep
 camera = PiCamera()
 
 camera.start_preview()
-sleep(10)
+camera.start_recording('/home/pi/video.h264')
+sleep(30)
+camera.stop_recording()
 camera.stop_preview()
