83916d212d6663a363133d8ef0973a1c6081b7c7
jspahnn <jsphann@intelliad.de>
2019-01-27 15:38:20 +0100

    Added lights!

---
+++ esp8266-light-module/application.lua
@@ -0,0 +1,80 @@
+--                     --
+--      Constants      --
+--                     --
+
+-- true == on, false == off
+GPIO_LED_PIN = 7
+GPIO_MAIN_CONTROL_STATUS = false;
+
+GPIO_LED_ON = gpio.LOW;
+GPIO_LED_OFF = gpio.HIGH;
+
+
+UNIQUE_CLIENT_ID = "ESP001"
+MQTT_TOPIC_STATUS = UNIQUE_CLIENT_ID .. "-status"
+MQTT_TOPIC_CMD = UNIQUE_CLIENT_ID .. "-cmd"
+MQTT_CMD_OPTION_ON = "1"
+MQTT_CMD_OPTION_OFF = "0"
+
+--                     --
+--      Globals        --
+--                     --
+local lastToggleTime = 0
+local isConnected = false
+
+--                     --
+--      Functions      --
+--                     --
+function setRelay(status)
+    -- true == on, false == off
+    if (status)then
+        gpio.write(GPIO_LED_PIN, GPIO_LED_ON)
+				print("ON");
+gpio.write(1, gpio.LOW);
+gpio.write(2, gpio.LOW);
+gpio.write(3, gpio.LOW);
+    else
+        gpio.write(GPIO_LED_PIN, GPIO_LED_OFF)
+				print("OFF");
+gpio.write(1, gpio.LOW);
+gpio.write(2, gpio.LOW);
+gpio.write(3, gpio.HIGH);
+    end
+end
+
+function toggle()
+		if (GPIO_MAIN_CONTROL_STATUS)then
+			GPIO_MAIN_CONTROL_STATUS = false;
+			setRelay(false)
+		else
+			GPIO_MAIN_CONTROL_STATUS = true;
+			setRelay(true)
+		end
+end
+
+--                       --
+--      Main Script      --
+--                       --
+
+-- Setup the GPIO pins
+print("Start the main loop for toggling")
+
+-- Pin definition
+local duration = 3000
+
+-- Initialising pin
+gpio.mode(GPIO_LED_PIN, gpio.OUTPUT)
+
+gpio.mode(1, gpio.OUTPUT)
+gpio.mode(2, gpio.OUTPUT)
+gpio.mode(3, gpio.OUTPUT)
+
+
+
+-- Create an interval
+tmr.alarm(0, duration, 1, function ()
+   toggle(); 
+end)
+
+
+
---
+++ esp8266-light-module/init.lua
@@ -0,0 +1,18 @@
+
+function startup()
+    if file.open("init.lua") == nil then
+        print("init.lua deleted or renamed")
+    else
+        print("Running")
+        file.close("init.lua")
+        -- the actual application is stored in 'application.lua'
+        print("-- IP addr --")
+        print(wifi.sta.getip())
+        dofile("application.lua")
+    end
+end
+
+
+print("You have 3 seconds to abort")
+print("Waiting...")
+tmr.create():alarm(3000, tmr.ALARM_SINGLE, startup)
