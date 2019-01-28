c07fd7b195e83e8e0816c73d959a7a148cf99470
Sascha Dienelt <sdienelt@intelliad.de>
2019-01-28 15:30:33 +0100

    update supervisor runner and coniguration

--- supervisor/runner.sh
+++ supervisor/runner.sh
@@ -2,21 +2,6 @@
 
 SCRIPT=$1
 
-if [ $SCRIPT == "camera" ]; then
-	/home/pi/IOCT
-	export PYTHONPATH=.
-	python3 sensors/camera/run.py
-fi
-
-if [ $SCRIPT == "temperature" ]; then
-	/home/pi/IOCT
-	export PYTHONPATH=.
-	python3 sensors/temperature/run.py
-fi
-
-if [ $SCRIPT == "magnet" ]; then
-	cd /home/pi/IOCT
-	export PYTHONPATH=.
-	python3 actuators/magnet/run.py
-fi
-
+cd /home/pi/IOCT
+export PYTHONPATH=.
+python3 "${SCRIPT}/run.py"
